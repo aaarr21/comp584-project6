3d923f7ff45ed20d7e8b4d7ec6b61ad1b4c7348c
aaarr21 <john102111@gmail.com>
2025-12-19 00:20:20 -0800

    day2

---
+++ day2/day2.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+// Read input lines
+const lines = fs.readFileSync('input.txt', 'utf8')
+  .trim()
+  .split('\n');
+
+let part1 = 0;
+let part2 = 0;
+
+for (const line of lines) {
+  // Example: "1-3 a: abcde"
+  const [range, letterPart, password] = line.split(' ');
+  const [min, max] = range.split('-').map(Number);
+  const letter = letterPart[0]; // remove ':'
+
+  // ---------- Part 1 ----------
+  const count = password.split('').filter(c => c === letter).length;
+  if (count >= min && count <= max) part1++;
+
+  // ---------- Part 2 ----------
+  const pos1 = password[min - 1] === letter;
+  const pos2 = password[max - 1] === letter;
+  if (pos1 !== pos2) part2++;   // XOR: exactly one true
+}
+
+console.log("Part 1 valid passwords:", part1);
+console.log("Part 2 valid passwords:", part2);
